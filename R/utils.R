47d2490215f9735149da38a69d324f64117e1da4
Heaghneyn <103440775+Heaghneyn@users.noreply.github.com>
2024-04-24 13:36:35 -0400

    add spatial folder param to preprocess_cdc

--- R/utils.R
+++ R/utils.R
@@ -1653,8 +1653,10 @@ hard_coded_cases <- function(df){
 #' Process POLIS data into analytic datasets needed for CDC
 #' @import cli sirfunctions dplyr readr lubridate stringr rio tidyr openxlsx stringi purrr pbapply lwgeom
 #' @param polis_data_folder str: location of the POLIS data folder, defaults to value stored from init_tidypolis
+#' @param polis_spatial_folder str: location of the POLIS spatial folder, defaults to value stored from init_tidypolis
 #' @return Outputs intermediary core ready files
-preprocess_cdc <- function(polis_data_folder = Sys.getenv("POLIS_DATA_CACHE")) {
+preprocess_cdc <- function(polis_data_folder = Sys.getenv("POLIS_DATA_CACHE"),
+                           polis_spatial_folder = Sys.getenv("POLIS_SPATIAL_CACHE")) {
 
   #Step 0 - create a CORE datafiles to combine folder and check for datasets before continuing with pre-p =========
   if(!tidypolis_io(io = "exists.dir", file_path = paste0(polis_data_folder, "/core_files_to_combine"))){
@@ -1762,8 +1764,7 @@ preprocess_cdc <- function(polis_data_folder = Sys.getenv("POLIS_DATA_CACHE")) {
 
   #Get geodatabase to auto-fill missing GUIDs
   cli::cli_process_start("Long district spatial file")
-  long.global.dist.01 <-
-    sirfunctions::load_clean_dist_sp(type = "long")
+  long.global.dist.01 <- tidypolis_io(io = "read", file_path = paste0(polis_spatial_folder, "/spatial/global.dist.rds"))
   cli::cli_process_done()
 
 
