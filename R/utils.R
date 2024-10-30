59bb9f1d0c30f2c3dbc9058caea0b988ec57a157
Nick Heaghney <103440775+Heaghneyn@users.noreply.github.com>
2024-10-30 12:39:40 -0400

    convert invalid dist shapes to tidypolis_io

--- R/utils.R
+++ R/utils.R
@@ -4876,7 +4876,15 @@ process_spatial <- function(gdb_folder,
 
   sf::st_geometry(invalid.dist.shapes) <- NULL
 
-  utils::write.csv(invalid.dist.shapes, file = paste0(output_folder, "/invalid_dist_shapes.csv"))
+  if(edav) {
+    tidypolis_io(io = "write", edav = T,
+                 file_path = paste0(output_folder, "/invalid_dist_shapes.csv"),
+                 obj = invalid.dist.shapes)
+  } else {
+    tidypolis_io(io = "write", edav = F,
+                 file_path = paste0(output_folder, "/invalid_dist_shapes.csv"),
+                 obj = invalid.dist.shapes)
+  }
 
   empty.dist <- global.dist.01 |>
     dplyr::mutate(empty = sf::st_is_empty(SHAPE)) |>
