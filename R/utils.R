0d88de657bd403897f15a6710e2af72f24e0b82f
Nick Heaghney <103440775+Heaghneyn@users.noreply.github.com>
2025-02-18 13:11:18 -0500

    update df, remove correct vars, account for gdb issues

--- R/utils.R
+++ R/utils.R
@@ -3215,7 +3215,7 @@ preprocess_cdc <- function(polis_data_folder = Sys.getenv("POLIS_DATA_CACHE")) {
   shapenames$match01 <- 1
 
   # matching by guid and marking unmatched provinces
-  afp.linelist.fixed <- dplyr::left_join(afp.linelist.01, shapes, by = c("Admin0GUID" = "ADM0_GUID", "Admin1GUID" = "GUID", "yronset" = "active.year.01"))
+  afp.linelist.fixed <- dplyr::left_join(afp.linelist.fixed, shapes, by = c("Admin0GUID" = "ADM0_GUID", "Admin1GUID" = "GUID", "yronset" = "active.year.01"))
   afp.linelist.fixed <- afp.linelist.fixed |>
     dplyr::mutate(wrongAdmin1GUID = ifelse(is.na(match) & !(is.na(admin1guid)), "yes", "no"))
 
@@ -3253,15 +3253,18 @@ preprocess_cdc <- function(polis_data_folder = Sys.getenv("POLIS_DATA_CACHE")) {
     dplyr::mutate(wrongAdmin2GUID = ifelse(is.na(match) & !(is.na(admin2guid)), "yes", "no"))
 
   # matching by name and fixing incorrect Admin2 guids which can be matched by name
-  afp.linelist.fixed.02 <- afp.linelist.fixed.02 |>
+  afp.linelist.fixed.03 <- afp.linelist.fixed.02 |>
+    dplyr::filter(wrongAdmin2GUID == "yes") |>
     dplyr::left_join(shapenames, by = c("place.admin.0" = "ADM0_NAME", "place.admin.1" = "ADM1_NAME", "place.admin.2" = "ADM2_NAME", "yronset" = "active.year.01"),
                      relationship = "many-to-many")
   afp.linelist.fixed.02 <- afp.linelist.fixed.02 |>
+    dplyr::filter(!epid %in% afp.linelist.fixed.03$epid) |>
+    dplyr::bind_rows(afp.linelist.fixed.03) |>
     dplyr::mutate(Admin2GUID = ifelse(wrongAdmin2GUID == "yes" & !is.na(match01), GUID, Admin2GUID))
 
 
   afp.linelist.fixed.02 <- afp.linelist.fixed.02 |>
-    dplyr::select(-match, -match01, -ADM1_GUID, -GUID)
+    dplyr::select(-match, -match01, -GUID)
 
   # Now afp.linelist.fixed.02 is identical to afp.linelist.01 but with incorrect guids fixed at
   # the district and province level. There are also two new columns added to indicate wether
