8e0a54024e81d6adabf1e8c5541f7b5d28f42f0f
Mohamed Yusuf <mohamedayusuf87@gmail.com>
2025-04-17 10:03:24 +0300

    Add functionality to read and write paraquet files

--- R/dal.R
+++ R/dal.R
@@ -1,11 +1,11 @@
-#DATA ACCESS LAYER
+# DATA ACCESS LAYER
 
 #' Tiypolis input and output
 #'
 #' @description
 #' Manages read/write/list/create/delete functions for tidypolis
 #'
-#' @import sirfunctions dplyr AzureStor readr stringr cli
+#' @import sirfunctions dplyr AzureStor readr stringr cli arrow
 #' @param obj `str` Object to be loaded into EDAV.
 #' @param io `str` read/write/list/exists/create/delete
 #' @param file_path `str` Absolute path of file.
@@ -116,8 +116,8 @@ tidypolis_io <- function(
         return(x)
       }
     } else {
-      if (!grepl("\\.rds$|\\.rda$|\\.csv$", file_path)) {
-        stop("At the moment only 'rds' 'rda' and 'csv' are supported for reading.")
+      if (!grepl("\\.rds$|\\.rda$|\\.csv$|\\.parquet$", file_path)) {
+        stop("At the moment only 'rds' 'rda' 'csv' and 'parquet' are supported for reading.")
       }
 
       if (grepl("\\.rds$", file_path)) {
@@ -130,6 +130,10 @@ tidypolis_io <- function(
 
       if (grepl("\\.csv$", file_path)) {
         return(readr::read_csv(file_path))
+      }
+
+      if (grepl("\\.parquet$", file_path)) {
+        return(arrow::read_parquet(file_path))
       }
     }
   }
@@ -147,8 +151,8 @@ tidypolis_io <- function(
         azcontainer = azcontainer
       )
     } else {
-      if (!grepl("\\.rds$|\\.rda$|\\.csv$", file_path)) {
-        stop("At the moment only 'rds' 'rda' and 'csv' are supported for reading.")
+      if (!grepl("\\.rds$|\\.rda$|\\.csv$|\\.parquet$", file_path)) {
+        stop("At the moment only 'rds' 'rda' 'csv' and 'parquet' are supported for reading.")
       }
 
       if (grepl("\\.rds$", file_path)) {
@@ -161,6 +165,10 @@ tidypolis_io <- function(
 
       if (grepl("\\.csv$", file_path)) {
         readr::write_csv(x = obj, file = file_path)
+      }
+
+      if (grepl("\\.parquet$", file_path)) {
+        arrow::write_parquet(x = obj, sink = file_path)
       }
     }
   }
